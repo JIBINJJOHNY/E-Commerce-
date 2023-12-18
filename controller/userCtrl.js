64bb93c9f3fcd3048d3ae9459b9cbeaf0dda276e
JIBINJJOHNY <jibinjjohny11@gmail.com>
2023-12-18 11:14:33 +0100

    Updated error handler

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,6 +1,6 @@
 const User = require('../models/userModel');
-
-const createUser = async (req, res) => {
+const asyncHandler = require("express-async-handler");
+const createUser = asyncHandler(async (req, res) => {
     const email = req.body.email;
     const findUser = await User.findOne({email:email});
     if (!findUser) {
@@ -8,14 +8,9 @@ const createUser = async (req, res) => {
         const newUser = await User.create(req.body);
         res.json(newUser);
     } else {
-        //User Already Exist
-        res.json({
-            msg: "User Already Exist",
-            success: false,
-
-        });
+       throw new Error('User Already Exists');
     }
-};
+});
 module.exports = {
     createUser
 }
